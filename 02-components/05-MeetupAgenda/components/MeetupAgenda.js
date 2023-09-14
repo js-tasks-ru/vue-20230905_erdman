9ccafa8d463e440bf51c51a07c76ddd16d12d5d0
Dmitry Eremeev <eremeev.dmitry@yandex.ru>
2023-09-14 20:53:37 +0300

    Добавлено решение задачи 02/05-MeetupAgenda

--- 02-components/05-MeetupAgenda/components/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -1,13 +1,28 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
 
+  components: {
+    MeetupAgendaItem,
+  },
+
+  props: {
+    agenda: {
+      type: Array,
+      required: true,
+    },
+  },
+
   template: `
     <ul class="agenda">
       <li class="agenda__item">
-        <!-- meetup agenda item -->
+        <MeetupAgendaItem
+          v-for="item in agenda"
+          :key="item.id"
+          :agendaItem="item"
+        />
       </li>
     </ul>`,
 });
