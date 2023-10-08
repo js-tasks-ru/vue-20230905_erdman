dd97206933cb3328fb527b308ba2f7f7058da64e
Dmitry Eremeev <eremeev.dmitry@yandex.ru>
2023-10-08 17:00:56 +0300

    Добавлено решение задачи 07/01-CounterButton

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,37 @@
 <template>
-  <button></button>
+  <button type="button" @click="handleClick">{{ localCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  data() {
+    return {
+      localCount: this.count,
+    };
+  },
+
+  watch: {
+    count(newCount) {
+      this.localCount = newCount;
+    },
+  },
+
+  methods: {
+    handleClick() {
+      this.localCount += 1;
+      this.$emit('update:count', this.localCount);
+    },
+  },
 };
 </script>
